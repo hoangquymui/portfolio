325cdf7afb832d09f0110ffdb5879e81c5f410e2
hoangquymui <hoangquymui.work@gmail.com>
2025-07-03 16:40:24 +0700

    Add main application structure with routing, components, and styles

---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import { ThemeToggle } from "@/components/ThemeToggle.jsx";
+import { StarBackground } from "@/components/StarBackground.jsx";
+import { NavBar } from "@/components/NavBar";
+import { HeroSection } from "@/components/HeroSection.jsx";
+
+export const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* Theme Toggle */}
+      <ThemeToggle />
+      {/* Backgound Effect */}
+      <StarBackground />
+      {/* NavBar */}
+      <NavBar />
+      {/* Main Content */}
+      <main>
+        <HeroSection />
+        
+      </main>
+
+      {/* Footer */}
+    </div>
+  );
+};
